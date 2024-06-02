cfb37506091a65a03ec6b11cd713943cb01fddb6
abdelwahab-JEmLa <156733337+abdelwahab-JEmLa@users.noreply.github.com>
2024-06-02 16:29:31 +0100

    Start

--- app/src/main/java/com/example/abdelwahabjemlajetpack/MainActivity.kt
+++ app/src/main/java/com/example/abdelwahabjemlajetpack/MainActivity.kt
@@ -22,8 +22,14 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -41,18 +47,36 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AbdelwahabJeMLaJetPackTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    MyMultiline(
-                        myText = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                    ScreenContent()
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    WellnessScreen()
                 }
             }
         }
     }
 }
+@Composable
+fun WellnessScreen(modifier: Modifier = Modifier) {
+    WaterCounter(modifier)
+}
 
+@Composable
+fun WaterCounter(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        var count by remember { mutableStateOf(0) }
+
+        if (count > 0) {
+            // This text is present if the button has been clicked
+            // at least once; absent otherwise
+            Text("You've had $count glasses.")
+        }
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
+            Text("Add one")
+        }
+    }
+}
 @Composable
 fun ScreenContent(modifier: Modifier = Modifier) {
 
@@ -137,6 +161,7 @@ fun MyMultiline(myText: String, modifier: Modifier = Modifier, fontSize: Int = 3
 fun GreetingPreview() {
     AbdelwahabJeMLaJetPackTheme {
     //    MyMultiline("Android ")
-        ScreenContent()
+     //   ScreenContent()
+        WellnessScreen()
     }
 }
