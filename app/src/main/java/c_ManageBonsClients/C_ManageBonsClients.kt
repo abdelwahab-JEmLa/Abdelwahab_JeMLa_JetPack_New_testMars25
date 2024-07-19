142ad9d82056b52dce2e5652d94a2add7af125ba
abdelwahab-JEmLa <156733337+abdelwahab-JEmLa@users.noreply.github.com>
2024-07-19 03:36:10 +0100

    ajout elements to page de debut

---
+++ app/src/main/java/c_ManageBonsClients/C_ManageBonsClients.kt
@@ -0,0 +1,11 @@
+package c_ManageBonsClients
+
+import androidx.compose.runtime.Composable
+import b_Edite_Base_Donne.ArticleDao
+
+@Composable
+fun C_ManageBonsClients(
+    articleDao: ArticleDao,
+) {
+
+}
