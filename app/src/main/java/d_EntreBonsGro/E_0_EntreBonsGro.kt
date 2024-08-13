d08d2db8c7c50d4bda2ec255488782e167b7178f
abdelwahab-JEmLa <156733337+abdelwahab-JEmLa@users.noreply.github.com>
2024-08-13 21:20:23 +0100

    TODO

--- app/src/main/java/d_EntreBonsGro/E_0_EntreBonsGro.kt
+++ app/src/main/java/d_EntreBonsGro/E_0_EntreBonsGro.kt
@@ -760,7 +760,7 @@ fun VoiceInputButton(
     }
 
     FloatingActionButton(
-        onClick = {
+        onClick = {//TODO fait que si aucune n ai entre de relence
             val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
                 putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
                 putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ar-DZ")
