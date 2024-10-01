be6d34c48631f34f5205754389ed8dc5399b356f
abdelwahab-JEmLa <156733337+abdelwahab-JEmLa@users.noreply.github.com>
2024-10-01 09:30:23 +0100

    1

--- app/src/main/java/b2_Edite_Base_Donne_With_Creat_New_Articls/Main.kt
+++ app/src/main/java/b2_Edite_Base_Donne_With_Creat_New_Articls/Main.kt
@@ -1,6 +1,5 @@
 package b2_Edite_Base_Donne_With_Creat_New_Articls
 
-
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
