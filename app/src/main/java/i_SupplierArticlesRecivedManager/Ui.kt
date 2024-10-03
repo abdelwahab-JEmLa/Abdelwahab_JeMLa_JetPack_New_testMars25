a4d721175d551bca9e9c35a44e4fb8e584ddb4d6
abdelwahab-JEmLa <156733337+abdelwahab-JEmLa@users.noreply.github.com>
2024-10-03 04:57:00 +0100

    Head gross

--- app/src/main/java/i_SupplierArticlesRecivedManager/Ui.kt
+++ app/src/main/java/i_SupplierArticlesRecivedManager/Ui.kt
@@ -1,40 +1,23 @@
 package i_SupplierArticlesRecivedManager
 
 
-import a_MainAppCompnents.BaseDonneECBTabelle
 import a_MainAppCompnents.HeadOfViewModels
 import a_MainAppCompnents.TabelleSupplierArticlesRecived
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.AddAPhoto
-import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -42,27 +25,19 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.capitalize
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.intl.Locale
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
-import b_Edite_Base_Donne.AutoResizedText
 import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
 import coil.size.Size
 import com.example.abdelwahabjemlajetpack.R
 import java.io.File
-
 
 
 @Composable
@@ -72,8 +47,6 @@ fun ArticleDetailWindow(
     viewModel: HeadOfViewModels,
     modifier: Modifier, onReloadTrigger: () -> Unit, relodeTigger: Int
 ) {
-    var displayeInOutlines by remember { mutableStateOf(true) }
-    var currentChangingField by remember { mutableStateOf("") }
 
     Dialog(
         onDismissRequest = onDismiss,
@@ -130,16 +103,14 @@ fun DisplayColorsCards(
         modifier = modifier.fillMaxWidth()
     ) {
         itemsIndexed(couleursList) { index, couleur ->
-            if (couleur != null) {
-                ColorCard(
-                    article,
-                    index,
-                    couleur,
-                    viewModel,
-                    onDismiss,
-                    onReloadTrigger,
-                    relodeTigger,)
-            }
+            ColorCard(
+                article,
+                index,
+                couleur,
+                viewModel,
+                onDismiss,
+                onReloadTrigger,
+                relodeTigger,)
         }
 
     }
@@ -183,49 +154,12 @@ private fun ColorCard(
                     reloadKey =relodeTigger
                 )
 
-                IconButton(
-                    onClick = { showDeleteDialog = true },
-                    modifier = Modifier.align(Alignment.TopEnd)
-                ) {
-                    Icon(Icons.Default.Delete, contentDescription = "Delete color")
-                }
-
-                IconButton(
-                    onClick = {
-                        viewModel.tempImageUri = viewModel.createTempImageUri(context)
-                        launcher.launch(viewModel.tempImageUri!!)
-                    },
-                    modifier = Modifier.align(Alignment.BottomEnd)
-                ) {
-                    Icon(Icons.Default.AddAPhoto, contentDescription = "Add photo")
-                }
             }
             Spacer(modifier = Modifier.height(8.dp))
             AutoResizedTextECB(text = couleur)
         }
     }
 
-    if (showDeleteDialog) {
-        AlertDialog(
-            onDismissRequest = { showDeleteDialog = false },
-            title = { Text("Confirm Deletion") },
-            text = { Text("Are you sure you want to delete this color?") },
-            confirmButton = {
-                Button(onClick = {
-                    showDeleteDialog = false
-                    viewModel.deleteColor(article, index + 1)
-                   if (index==0) onDismiss()
-                }) {
-                    Text("Confirm")
-                }
-            },
-            dismissButton = {
-                Button(onClick = { showDeleteDialog = false }) {
-                    Text("Cancel")
-                }
-            }
-        )
-    }
 }
 
 @Composable
@@ -266,125 +200,3 @@ fun DisplayeImage(
         contentScale = ContentScale.Fit
     )
 }
-@Composable
-private fun AddColorCard(onClick: () -> Unit) {
-    Card(
-        modifier = Modifier
-            .height(200.dp)
-            .aspectRatio(1f)
-            .padding(4.dp)
-    ) {
-        Box(
-            contentAlignment = Alignment.Center,
-            modifier = Modifier.fillMaxSize()
-        ) {
-            IconButton(onClick = onClick) {
-                Icon(
-                    imageVector = Icons.Default.Add,
-                    contentDescription = "Add new color",
-                    modifier = Modifier.size(48.dp)
-                )
-            }
-        }
-    }
-}
-
-@Composable
-fun OutlineTextECB(
-    columnToChange: String,
-    abbreviation: String,
-    currentChangingField: String,
-    article: BaseDonneECBTabelle,
-    viewModel: HeadOfViewModels,
-    modifier: Modifier = Modifier,
-    onValueChanged: (String) -> Unit
-) {
-
-    var textFieldValue by remember { mutableStateOf(
-        article.getColumnValue(columnToChange)?.toString()?.replace(',', '.') ?: "") }
-    val textValue = if (currentChangingField == columnToChange) textFieldValue else ""
-    val labelValue = article.getColumnValue(columnToChange)?.toString()?.replace(',', '.') ?: ""
-    val roundedValue = try {
-        labelValue.toDouble()
-            .let { if (it % 1 == 0.0) it.toInt().toString() else String.format("%.1f", it) }
-    } catch (e: NumberFormatException) {
-        labelValue
-    }
-
-    val keyboardController = LocalSoftwareKeyboardController.current
-
-    OutlinedTextField(
-        value = textValue,
-        onValueChange = { newValue ->
-            textFieldValue = newValue.replace(',', '.')
-            viewModel.updateAndCalculateAuthersField(textFieldValue, columnToChange, article)
-            onValueChanged(columnToChange)
-        },
-        label = {
-            AutoResizedText(
-                text = "$abbreviation$roundedValue",
-                color = Color.Red,
-                modifier = Modifier.fillMaxWidth()
-            )
-        },
-        textStyle = TextStyle(color = Color.Blue, textAlign = TextAlign.Center, fontSize = 14.sp),
-        modifier = modifier
-            .fillMaxWidth()
-            .height(65.dp),
-        keyboardOptions = KeyboardOptions(
-            keyboardType = KeyboardType.Number,
-            imeAction = ImeAction.Done
-        ),
-        keyboardActions = KeyboardActions(
-            onDone = {
-                keyboardController?.hide()
-            }
-        )
-    )
-}
-
-@Composable
-fun CalculationButtons(
-    article: BaseDonneECBTabelle,
-    viewModel: HeadOfViewModels,
-    modifier: Modifier
-) {
-    Row(
-        modifier = modifier
-            .fillMaxWidth()
-            ,
-        horizontalArrangement = Arrangement.SpaceEvenly
-    ) {
-        Button(onClick = {
-            viewModel.updateAndCalculateAuthersField(
-                (article.monPrixAchat / article.nmbrUnite).toString(),
-                "monPrixAchat",
-                article
-            )
-        }) { Text("/") }
-        Button(onClick = {
-            viewModel.updateAndCalculateAuthersField(
-                (article.monPrixAchat * article.nmbrUnite).toString(),
-                "monPrixAchat",
-                article
-            )
-        }) { Text("*") }
-    }
-}
-
-@Composable
-fun ArticleToggleButton(
-    article: BaseDonneECBTabelle,
-    viewModel: HeadOfViewModels,
-    modifier: Modifier
-) {
-    Button(
-        onClick = { viewModel },
-        colors = ButtonDefaults.buttonColors(containerColor = if (article.affichageUniteState) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.error),
-        modifier = modifier
-            .fillMaxWidth()
-    ) {
-        Text(text = if (article.affichageUniteState) "Cacher les Unités" else "Afficher les Unités")
-    }
-}
-
