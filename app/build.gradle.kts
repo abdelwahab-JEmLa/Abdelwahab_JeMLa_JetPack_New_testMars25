9c0bed3ecc5a86d0ed352ae988445fa386cfbdd0
abdelwahab-JEmLa <156733337+abdelwahab-JEmLa@users.noreply.github.com>
2024-10-19 16:03:53 +0100

    1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.abdelwahabclientjetpack"
+        applicationId = "com.example.abdelwahabjemlajetpack"
         minSdk = 26
         targetSdk = 34
         versionCode = 1
