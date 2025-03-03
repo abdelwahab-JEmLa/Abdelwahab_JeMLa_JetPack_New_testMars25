19650642cd6a365edc716df4a6465e779c569c50
abdelwahab-JEmLa <156733337+abdelwahab-JEmLa@users.noreply.github.com>
2025-03-03 09:52:44 +0100

    save

---
+++ app/src/main/java/Z_MasterOfApps/Z_AppsFather/Kotlin/Learn/A_Koin/MyApplication.kt
@@ -0,0 +1,18 @@
+package Z_MasterOfApps.Z_AppsFather.Kotlin.Learn.A_Koin
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidLogger()
+            androidContext(this@MyApplication)
+            modules(appModule)
+        }
+    }
+}
